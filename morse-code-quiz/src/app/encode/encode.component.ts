e30d7acdc79ba1c71369a0b993c8f2e53ac20a2a
Bilal Makic <b.makic@HTBLALeonding.onmicrosoft.com>
2022-11-01 21:43:13 +0100

    formatting

--- morse-code-quiz/src/app/encode/encode.component.ts
+++ morse-code-quiz/src/app/encode/encode.component.ts
@@ -3,19 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-encode',
   templateUrl: './encode.component.html',
-  styleUrls: ['./encode.component.css']
+  styleUrls: ['./encode.component.css'],
 })
 export class EncodeComponent {
-
   public encodedText: string;
   public inputText: string;
 
   constructor() {
-    this.inputText = "";
-    this.encodedText = "";
+    this.inputText = '';
+    this.encodedText = '';
   }
 
-  public encode(): void{
+  public encode(): void {
     const morseCode = [
       /* A */ '.-',
       /* B */ '-...',
@@ -45,23 +44,23 @@ export class EncodeComponent {
       /* Z */ '--..',
     ];
 
-    this.encodedText = "";
+    this.encodedText = '';
     this.inputText = this.inputText.trim();
 
-    for(let ch of this.inputText){
-      if(ch.match(/[A-Z]/)){
-       let alphabetIndex = ch.charCodeAt(0) - "A".charCodeAt(0)
-       this.encodedText += morseCode[alphabetIndex];
-       this.encodedText+= " ";
-      }
-      else if(ch === " " && this.encodedText[this.encodedText.length - 2] !== "/"){
-        this.encodedText += "/ ";
-      }
-      else if (ch !== " "){
+    for (let ch of this.inputText) {
+      if (ch.match(/[A-Z]/)) {
+        let alphabetIndex = ch.charCodeAt(0) - 'A'.charCodeAt(0);
+        this.encodedText += morseCode[alphabetIndex];
+        this.encodedText += ' ';
+      } else if (
+        ch === ' ' &&
+        this.encodedText[this.encodedText.length - 2] !== '/'
+      ) {
+        this.encodedText += '/ ';
+      } else if (ch !== ' ') {
         this.encodedText += ch;
       }
     }
     this.encodedText = this.encodedText.trim();
   }
-
 }
